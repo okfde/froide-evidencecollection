c4efb5fb3b4b2ded7cb95f87fe384f368256eda1
Kara Engelhardt <kara.engelhardt@okfn.de>
2024-10-17 14:31:12 +0200

    Show export link in search view

--- froide_evidencecollection/urls.py
+++ froide_evidencecollection/urls.py
@@ -4,6 +4,6 @@ from .views import EvidenceDetailView, EvidenceExportView, EvidenceListView
 
 urlpatterns = [
     path("", EvidenceListView.as_view(), name="evidence-list"),
-    path("export/", EvidenceExportView.as_view(), name="evidence-list"),
+    path("export/", EvidenceExportView.as_view(), name="evidence-export"),
     path("<int:pk>/", EvidenceDetailView.as_view(), name="evidence-detail"),
 ]
