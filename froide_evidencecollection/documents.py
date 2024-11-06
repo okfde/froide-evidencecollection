7ce5a1174b02f43387d790b87c2ff1323f8cdf94
Kara Engelhardt <kara.engelhardt@okfn.de>
2024-11-06 16:31:19 +0100

    Add text analyzer to fts fields

--- froide_evidencecollection/documents.py
+++ froide_evidencecollection/documents.py
@@ -2,12 +2,18 @@ from django_elasticsearch_dsl import Document, fields
 
 from froide.helper.search import (
     get_index,
+    get_search_analyzer,
+    get_search_quote_analyzer,
+    get_text_analyzer,
 )
 
 from .models import Evidence, Person
 
 evidence_index = get_index("evidence")
 person_index = get_index("person")
+analyzer = get_text_analyzer()
+search_analyzer = get_search_analyzer()
+search_quote_analyzer = get_search_quote_analyzer()
 
 
 @evidence_index.document
@@ -15,13 +21,16 @@ class EvidenceDocument(Document):
     type = fields.IntegerField(attr="type_id")
     area = fields.IntegerField(attr="area_id")
     person = fields.IntegerField(attr="person_id")
-    person_name = fields.TextField()
+    person_name = fields.TextField(
+        analyzer=analyzer,
+    )
     quality = fields.IntegerField(attr="quality_id")
     public_body = fields.IntegerField(attr="source__public_body_id")
 
     class Django:
         model = Evidence
         fields = ["title", "description", "note", "date"]
+        fts_fields = ["title", "description", "note"]
 
     def get_queryset(self):
         return (
@@ -32,6 +41,18 @@ class EvidenceDocument(Document):
 
     def prepare_person_name(self, obj: Evidence):
         return obj.person.name
+
+    @classmethod
+    def to_field(cls, field_name, model_field):
+        if field_name in cls.Django.fts_fields:
+            return fields.TextField(
+                analyzer=analyzer,
+                search_analyzer=search_analyzer,
+                search_quote_analyzer=search_quote_analyzer,
+                index_options="offsets",
+            )
+        else:
+            return super().to_field(field_name, model_field)
 
 
 @person_index.document
