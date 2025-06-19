d6a50cb925b15b6fb6be2ec7ecaa9f9be73608a2
Anett Seeker <noeddl@users.noreply.github.com>
2025-06-19 10:03:46 +0200

    ♻️ Move definition of selectable regions to settings

--- froide_evidencecollection/regions.py
+++
@@ -1,9 +0,0 @@
-from django.db.models import Q
-
-from froide.georegion.models import GeoRegion
-
-
-def selectable_regions():
-    return GeoRegion.objects.filter(Q(kind="state") | Q(name="Deutschland")).order_by(
-        "kind", "name"
-    )
