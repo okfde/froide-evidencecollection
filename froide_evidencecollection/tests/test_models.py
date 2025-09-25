02b325c94942bcaa357800abb07a6e08e95e23b4
Anett Seeker <noeddl@users.noreply.github.com>
2025-09-25 10:42:26 +0200

    🗃️ Add last_synced_state field to SyncableModel

--- froide_evidencecollection/tests/test_models.py
+++ froide_evidencecollection/tests/test_models.py
@@ -2,7 +2,7 @@ import pytest
 
 from froide_evidencecollection.models import SyncableModel
 
-from .factories import syncable_model_factories
+from .factories import RoleFactory, syncable_model_factories
 
 
 @pytest.mark.django_db
@@ -15,6 +15,7 @@ class TestSyncableModel:
         assert instance.sync_uuid is not None
         assert instance.synced_at is None
         assert instance.is_synced is False
+        assert instance.last_synced_state == {}
 
     @pytest.mark.parametrize("factory", syncable_model_factories)
     def test_syncable_model_saving(self, factory):
@@ -26,21 +27,26 @@ class TestSyncableModel:
         assert instance.updated_at > updated_at
         assert instance.synced_at is None
         assert instance.is_synced is False
+        assert instance.last_synced_state == {}
 
-        # Save with sync=True updates synced_at and is_synced.
+        # Save with sync=True updates synced_at, is_synced, and last_synced_state.
         updated_at = instance.updated_at
         instance.save(sync=True)
         assert instance.updated_at > updated_at
         assert instance.synced_at == instance.updated_at
         assert instance.is_synced is True
+        assert instance.last_synced_state != {}
 
-        # Another normal save does not change synced_at or is_synced.
+        # Another normal save does not change synced_at or last_synced_state.
+        # The instance is out of sync now.
         updated_at = instance.updated_at
         synced_at = instance.synced_at
+        last_synced_state = instance.last_synced_state
         instance.save()
         assert instance.updated_at > updated_at
         assert instance.synced_at == synced_at
-        assert instance.is_synced is True
+        assert instance.is_synced is False
+        assert instance.last_synced_state == last_synced_state
 
     @pytest.mark.parametrize("factory", syncable_model_factories)
     def test_syncable_model_mark_synced(self, factory):
@@ -53,7 +59,7 @@ class TestSyncableModel:
         assert instance.updated_at == updated_at
         assert instance.is_synced is True
 
-    @pytest.mark.parametrize("factory", syncable_model_factories)
+    @pytest.mark.parametrize("factory", [RoleFactory])
     def test_syncable_model_update_without_sync(self, factory):
         instance = factory()
         sync_uuid = instance.sync_uuid
@@ -66,8 +72,9 @@ class TestSyncableModel:
         assert instance.updated_at > updated_at
         assert instance.synced_at is None
         assert instance.is_synced is False
+        assert instance.last_synced_state == {}
 
-    @pytest.mark.parametrize("factory", syncable_model_factories)
+    @pytest.mark.parametrize("factory", [RoleFactory])
     def test_syncable_model_update_with_sync(self, factory):
         instance = factory()
         sync_uuid = instance.sync_uuid
@@ -80,3 +87,4 @@ class TestSyncableModel:
         assert instance.updated_at > updated_at
         assert instance.synced_at == instance.updated_at
         assert instance.is_synced is True
+        assert instance.last_synced_state["name"] == "Updated Name"
